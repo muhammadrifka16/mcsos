0f9859310693931e979e42ad18045e02414651a4
Muhammad Rifka Z <muhammadrifka16@gmail.com>
2026-05-08 21:54:06 +0700

    M3 panic path logging gdb and disassembly audit

--- kernel/core/kmain.c
+++ kernel/core/kmain.c
@@ -1,27 +1,19 @@
 #include <stdint.h>
-
 #include <mcsos/arch/cpu.h>
-
 #include <mcsos/kernel/log.h>
 #include <mcsos/kernel/panic.h>
 #include <mcsos/kernel/version.h>
 
-extern uint8_t __kernel_start;
-extern uint8_t __kernel_end;
+extern char __kernel_start[];
+extern char __kernel_end[];
 
-static void m3_selftest(void)
-{
-    KERNEL_ASSERT(&__kernel_start != (uint8_t *)0);
-    KERNEL_ASSERT(&__kernel_end != (uint8_t *)0);
-    KERNEL_ASSERT(&__kernel_end >= &__kernel_start);
-
+static void m3_selftest(void) {
+    KERNEL_ASSERT(__kernel_end > __kernel_start);
+    KERNEL_ASSERT(sizeof(uintptr_t) == 8u);
     log_writeln("[M3] selftest: basic invariants passed");
 }
 
-void kmain(void)
-{
-    uint64_t rflags;
-
+void kmain(void) {
     log_init();
 
     log_write(MCSOS_NAME);
@@ -31,21 +23,25 @@ void kmain(void)
     log_write(MCSOS_MILESTONE);
     log_writeln(" kernel entered");
 
-    log_key_value_hex64(
-        "kernel_start",
-        (uint64_t)(uintptr_t)&__kernel_start
-    );
+    log_key_value_hex64("kernel_start",
+        (uint64_t)(uintptr_t)__kernel_start);
 
-    log_key_value_hex64(
-        "kernel_end",
-        (uint64_t)(uintptr_t)&__kernel_end
-    );
+    log_key_value_hex64("kernel_end",
+        (uint64_t)(uintptr_t)__kernel_end);
 
-    rflags = cpu_read_rflags();
-
-    log_key_value_hex64("rflags", rflags);
+    log_key_value_hex64("rflags",
+        cpu_read_rflags());
 
     m3_selftest();
 
-    KERNEL_PANIC("intentional M3 panic path", 0xDEADBEEFu);
+#ifdef MCSOS_M3_TRIGGER_PANIC
+    KERNEL_PANIC(
+        "intentional M3 panic test",
+        0x4D43534F533033u
+    );
+#else
+    log_writeln("[M3] panic path installed; intentional panic disabled");
+    log_writeln("[M3] ready for QEMU smoke test and GDB audit");
+    cpu_halt_forever();
+#endif
 }
